4de2762e4d726491fd5cd41621f1a9ec3554226f
Arek Ogórek <ogurkowo@gmail.com>
2020-07-03 22:54:16 +0200

    Moduł magazyn

--- src/components/AddMove.js
+++ src/components/AddMove.js
@@ -7,62 +7,108 @@ import "react-datepicker/dist/react-datepicker.css";
 import pl from 'date-fns/locale/pl';
 registerLocale('pl', pl);
 
-const AddMove = () => {
+const AddMove = ({onClick}) => {
     const [show, setShow] = useState(false);
     const db = firebase.firestore();
     const [startDate, setStartDate] = useState(new Date());
     const [product, setProduct] = useState("");
     const [listOfProducts, setListOfProducts] = useState([]);
-    const move = ["Sprzedaż", "Dostawa", "Wysyłka", "R+", "R-", "Gratis", "Strata", "Próbka"];
+    const move = ["Dostawa", "Wysyłka", "R+", "R-", "Strata"];
     const [selectMove, setSelectMove] = useState("");
     const [seller, setSeller] = useState("");
     const [users, setUsers] = useState([]);
     const [notes,setNotes] = useState("");
     const [quantity, setQuantity] = useState("");
+    const [counters, setCounters] = useState("");
+
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
     useEffect(() => {
-        db.collection('products').get()
+        db.collection('products')
+            .get()
             .then((snapshot) => {
                 snapshot.forEach((doc) => {
                     setListOfProducts(prevState => [...prevState, doc.data()]);
-                    // console.log(doc.data())
                 });
             })
             .catch((err) => {
                 console.log('Błąd bazy danych: ', err);
             });
-        db.collection('users').get()
+        db.collection('users')
+            .get()
             .then((snapshot) => {
                 snapshot.forEach((doc) => {
                     setUsers(prevState => [...prevState, doc.data()]);
-                    // console.log(doc.data())
                 });
             })
             .catch((err) => {
                 console.log('Błąd bazy danych: ', err);
             });
+        db.collection('counters')
+            .doc('stock').get()
+            .then(result => {
+                setCounters(result.data())});
 
     },[db]);
 
+    const handleCheckList = ()=> {
+        if (typeof onClick === "function") {
+            onClick();
+        }
+    };
+
     const handleClick = (e) => {
         e.preventDefault();
+        const newCounter ={
+            stockDocs: counters.stockDocs + 1,
+            stockIDs: counters.stockIDs + 1
+        };
+
+        let id = "";
+            listOfProducts.forEach((e) => {
+            if (e.name === product) {
+                id = e.id
+            }
+        });
+
+        if (selectMove === "Dostawa" || selectMove === "R+") {
+            db.collection('products')
+                .doc(`${id}`)
+                .update({stock: firebase.firestore.FieldValue.increment(Number(quantity))})
+                .catch(error=>console.log(error));
+        } else if (selectMove === "Wysyłka" || selectMove === "R-" || selectMove === "Strata") {
+            db.collection('products')
+                .doc(`${id}`)
+                .update({stock: firebase.firestore.FieldValue.increment( - Number(quantity))})
+                .catch(error=>console.log(error));
+        }
         const newMove = {
+            id: counters.stockIDs + 1,
             date: startDate.toISOString().substring(0, 10),
             name: product,
             quantity: quantity,
             typeOfMove: selectMove,
             seller: seller,
             notes: notes
-        }
+        };
+        db.collection('counters')
+            .doc('stock')
+            .update(newCounter)
+            .catch(error=>console.log(error));
+
         db.collection('stock')
-            .add(newMove)
-            .then(() => handleClose())
+            .doc(`${counters.stockDocs + 1}`)
+            .set(newMove)
+            .then(() => {
+                handleClose();
+                handleCheckList();
+            })
             .catch(error => console.log(error))
     };
 
+    const isInvalid = selectMove !== 'Wysyłka';
 
     return (
         <>
@@ -83,26 +129,50 @@ const AddMove = () => {
                             dateFormat="yyyy.MM.dd"
                         />
                     </span>
-                    <select value={product} onChange={e => setProduct(e.target.value)}>
+                    <select
+                        value={product}
+                        onChange={e => setProduct(e.target.value)}
+                    >
                         <option>Produkt</option>
                         {listOfProducts.map((e) => <option key={e.id}>{e.name}</option> )}
                     </select>
-                    <input placeholder="ilość" value={quantity} onChange={e => setQuantity(e.target.value)}/>
-                    <select value={selectMove} onChange={e => setSelectMove(e.target.value)}>
+                    <input
+                        placeholder="ilość"
+                        value={quantity}
+                        onChange={e => setQuantity(e.target.value)}
+                    />
+                    <select
+                        value={selectMove}
+                        onChange={e => setSelectMove(e.target.value)}
+                    >
                         <option>Ruch</option>
                         {move.map((e,i) => <option key={i}>{e}</option> )}
                     </select>
-                    <select value={seller} onChange={e => setSeller(e.target.value)}>
+                    <select
+                        value={seller}
+                        onChange={e => setSeller(e.target.value)}
+                        disabled={isInvalid}
+                    >
                         <option>Handlowiec</option>
                         {users.map((e,i) => <option key={i}>{e.firstname} {e.lastname}</option> )}
                     </select>
-                    <input placeholder="uwagi" value={notes} onChange={e => setNotes(e.target.value)}/>
+                    <input
+                        placeholder="uwagi"
+                        value={notes}
+                        onChange={e => setNotes(e.target.value)}
+                    />
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="outline-secondary" onClick={handleClose}>
+                    <Button
+                        variant="outline-secondary"
+                        onClick={handleClose}
+                    >
                         Anuluj
                     </Button>
-                    <Button variant="outline-primary" onClick={handleClick}>
+                    <Button
+                        variant="outline-primary"
+                        onClick={handleClick}
+                    >
                         Zapisz i zamknij
                     </Button>
                 </Modal.Footer>
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { DataGrid, GridColumn, NumberBox, ComboBox } from 'rc-easyui';
+import { DataGrid, GridColumn } from 'rc-easyui';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import AddMove from "./AddMove";
@@ -7,12 +7,7 @@ import AddMove from "./AddMove";
 const  Stock = () => {
     const [data, setData] = useState([]);
     const db =firebase.firestore();
-    // const [operators, setOperators] = useState(["nofilter", "equal", "notequal", "less", "greater"]);
-    // const [status, setStatus] = useState([
-    //     { value: null, text: "All" },
-    //     { value: "P", text: "P" },
-    //     { value: "N", text: "N" }
-    // ]);
+
     useEffect(()=> {
         db.collection('stock').get()
             .then(snapshot => {
@@ -25,35 +20,30 @@ const  Stock = () => {
             });
     },[db]);
 
+    const refreshList = () => {
+        setData([]);
+        db.collection('stock').get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+                    setData(prevState => [...prevState, doc.data()])
+                })
+            })
+            .catch((err) => {
+                console.log('Błąd bazy danych: ', err);
+            });
+    };
+
      return (
         <main className="main-container">
             <section className="stock-table box">
-            <AddMove />
+            <AddMove onClick={refreshList}/>
                 <DataGrid filterable data={data} columnResizing>
-                    <GridColumn field="date" title="Data" width="6%" sortable></GridColumn>
-                    <GridColumn field="name" title="Nazwa produktu" width="25%" sortable></GridColumn>
-                    <GridColumn field="quantity" title="Ilość" width="5%" sortable
-                                // align="right"
-                                // filterOperators={operators}
-                                // filter={() => <NumberBox></NumberBox>}
-                    />
-                    <GridColumn field="typeOfMove" title="Typ ruchu" width="10%" sortable
-                                // align="right"
-                                // filterOperators={operators}
-                                // filter={() => <NumberBox></NumberBox>}
-                    />
-                    <GridColumn field="seller" title="Handlowiec" width="15%" sortable></GridColumn>
-                    <GridColumn field="notes" title="Uwagi" width="16%" sortable></GridColumn>
-                    {/*<GridColumn field="confirm" title="Zatwierdzenie" width="7%"*/}
-                    {/*            // align="center"*/}
-                    {/*            // filter={() => (*/}
-                    {/*            //     <ComboBox*/}
-                    {/*            //         data={status}*/}
-                    {/*            //         editable={false}*/}
-                    {/*            //         inputStyle={{ textAlign: 'center' }}*/}
-                    {/*            //     />*/}
-                    {/*            // )}*/}
-                    {/*// />*/}
+                    <GridColumn field="date" title="Data" width="6%" sortable/>
+                    <GridColumn field="name" title="Nazwa produktu" width="25%" sortable/>
+                    <GridColumn field="quantity" title="Ilość" width="5%" sortable />
+                    <GridColumn field="typeOfMove" title="Typ ruchu" width="10%" sortable />
+                    <GridColumn field="seller" title="Handlowiec" width="15%" sortable/>
+                    <GridColumn field="notes" title="Uwagi" width="16%" sortable/>
                 </DataGrid>
             </section>
         </main>
